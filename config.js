620cc07ae14eaa6fa474a638ddf4a312b7700f6f
GHOST-ID-MAX <ghostid2007@gmail.com>
2025-03-24 09:33:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
 
 //=======[dependencies]====================//
-global.SESSION_ID = process.env.SESSION_ID || "";
+global.SESSION_ID = process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0YxT2tMZGdkOUtzb2UrZW5wNlZiOEFxaW5GWlNpRlFNZmR4K3pXc0Ftbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiN1htREExcnRsN1RMcHhScEg1Q1J5cEQ2S2R0eHpZeVdZTEVtQlJaSXBUWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJhRU9UWTFzZFA2Q24rUjZ3eVFJWjU3MW1DbUZnRXpRTG5TVzY2K2xWam5rPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJBdXZoNTd5MnNGUXNLWWJMUUY5S3ZxYlc4YUJUVHVoTzhZNm8yWkxSckdjPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InNMcU05cWhTU0MrcGY2em9uMkd4OXhjQXpmWVl2Ui9rWUlHOTRKQ3BzWE09In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InhRVGhmdzZHUXVxR3VyUGhwa1VyY0tKYmxqa0w0TVhwaHAxUDd4cWpxQXc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiR0NpU2VhUG9FRGtUUmt6bWNNSjlUNklMK3Bqb1F2eXhMT2syL2psaGhrcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSVRMYjkyei9GM0cvOExUOG40TWQzU3ZHVCtqRjdUOGNyd0ZtVkpWcndXVT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkFFTlV0Vzl0YjFKQ0I4K3QybWVSNEhTSXV4V2hSWWhMTDFydjlDdnlXUnM4QlE2N0NyeDVja3VNU1Vnc2dsZmtIeU9jb1BTNDFXSEM2a1JVbE93YWh3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NDYsImFkdlNlY3JldEtleSI6IitGeFZ1NlIrQnlWZDVyRmtRWC9qSi9JYmg4MEFXZVNFMTB0bUQ1UUVNcUk9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6Ii1SMkdjNlFyU2xLVGNJcGpJUU0yVlEiLCJwaG9uZUlkIjoiYTRmNDQ2YmMtZmZiYy00OTU4LTgzMmYtMGRiN2UwNDliOTI3IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlR1TkU4ZjNGdEdvdUVTdm1FS2RKVGplamEwWT0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJOQTZ6enFzcmg3eGd1dDNqaFNqVHBYK2YzMFU9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiUFI5ODhDSDciLCJtZSI6eyJpZCI6Ijk0Nzg3NDM4OTI5OjIyQHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNPK2luUVVRNUxHRHZ3WVlBeUFBS0FBPSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJaMjNTN1pVZHBzUFhLL20vL01zRlI3a01INVNRMzh0dXZWcmtpdS9pcVI4PSIsImFjY291bnRTaWduYXR1cmUiOiJ2RG1uM0xqU2dwcG1sMkNuWWkzY3FIRG1MMFh1cWJCeWhyY2o4M1NDelp3bjIwWFI1Qnk0UG1JSmN4K2pidlprN21XZXE1S3V6dXd5MEZlVWp5Y3dEdz09IiwiZGV2aWNlU2lnbmF0dXJlIjoieFpWM21RK2dpaU1kQUJTdGVTcVlIQld6Nm1lZzg0S3c1QlRhWjhFVExZcktJY3NHTklCT3ZMSGp2YUdVT0l1V2FvSnFyczdqUEpXdDArVE5CZnd3aHc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5NDc4NzQzODkyOToyMkBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJXZHQwdTJWSGFiRDF5djV2L3pMQlVlNURCK1VrTi9MYnIxYTVJcnY0cWtmIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzQyNzg4ODQ4LCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUEwZiJ9";
 global.MONGODB = process.env.MONGODB_URI || "";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.sudo = process.env.SUDO
@@ -16,10 +16,10 @@ global.owner = process.env.OWNER_NUMBER
 global.THUMB_IMAGE =
   process.env.THUMB_IMAGE ||
   process.env.IMAGE ||
-  "https://i.postimg.cc/FssKzLK7/20240622-140407.jpg,https://i.postimg.cc/FssKzLK7/20240622-140407.jpg";
+  "https://media-hosting.imagekit.io//c14a69e57d6d4264/IMG-20250324-WA0015.jpg?Expires=1837395683&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=gHC7uDSwPRwTHqPUNNFbT-8E01DAZbFV1TxE8Nzj1hj6wrhAlud6-x9Ij~5PcYZVW-Qtf59ExzmifFq-BwQX5lhaTm9vV7MyUM6zOvFSmqff~qsrBcfmMXppp-xd8U09XOCN~WzpFtXpQ3mld5mT-9CrgTxTmGSGCcFQrLbyZ~CYFBa2Kc6nCmSYP1~DJ58BeOI30DhHqUYKl7dM7I2Qq1gC-n1dn7Fyc9FqP4PPEWsg74hMeHMYX1F1ChuAQj2dRMyyfgDoVCEXDWtDkg8D1PNN6KnRYWq0Enyhxjb1gKHENLrgyzMZ2q0pGIt0jLz6e32tihD7dg9KJ68LmDYjkw__";
 global.userImages =
   process.env.USER_IMAGES ||
-  "https://i.postimg.cc/3wrf9ccK/IMG-20240804-WA0000.jpg";
+  "https://media-hosting.imagekit.io//c14a69e57d6d4264/IMG-20250324-WA0015.jpg?Expires=1837395683&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=gHC7uDSwPRwTHqPUNNFbT-8E01DAZbFV1TxE8Nzj1hj6wrhAlud6-x9Ij~5PcYZVW-Qtf59ExzmifFq-BwQX5lhaTm9vV7MyUM6zOvFSmqff~qsrBcfmMXppp-xd8U09XOCN~WzpFtXpQ3mld5mT-9CrgTxTmGSGCcFQrLbyZ~CYFBa2Kc6nCmSYP1~DJ58BeOI30DhHqUYKl7dM7I2Qq1gC-n1dn7Fyc9FqP4PPEWsg74hMeHMYX1F1ChuAQj2dRMyyfgDoVCEXDWtDkg8D1PNN6KnRYWq0Enyhxjb1gKHENLrgyzMZ2q0pGIt0jLz6e32tihD7dg9KJ68LmDYjkw__";
 ///===========[global iMPORTS]====================//
 
 module.exports = {
@@ -27,11 +27,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`RCD-MD`",
-  author: process.env.PACK_AUTHER || "RCD-MD",
-  packname: process.env.PACK_NAME || "RCD",
-  botname: process.env.BOT_NAME || "RCD-MD",
-  ownername: process.env.OWNER_NAME || "RCD",
+  caption: process.env.CAPTION || "`GHOST-MD`",
+  author: process.env.PACK_AUTHER || "GHOST-MD",
+  packname: process.env.PACK_NAME || "GHOST ID",
+  botname: process.env.BOT_NAME || "GHOST-MD",
+  ownername: process.env.OWNER_NAME || "GHOST ID",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
